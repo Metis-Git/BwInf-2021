bd2a05424c8b8d34d94340ff18ff29a47c88f8a3
Bruno Hoffmann <robotix000@gmail.com>
2021-10-08 17:18:21 +0200

    jetzt kann man das mehrfach machen, toll oder?

--- Aufgabe2/BwInf21-Aufgabe2/BwInf21-Aufgabe2.cpp
+++ Aufgabe2/BwInf21-Aufgabe2/BwInf21-Aufgabe2.cpp
@@ -10,42 +10,13 @@ struct Hotel {
     int time;
 };
 
-double bestPathRating = 0.0;
-int bestPathTime = 10;
 int* bestPath;
+double bestPathRating;
+int bestPathTime;
 
 Hotel* hotels;
-int endPointTime = 0;
-int hotelCount = 0;
-
-void loadFromFile(std::string path) {
-    std::ifstream fileStream(path);
-    std::string line;
-    int lineCount = 0;
-
-    while (getline(fileStream, line)) {
-        if (lineCount == 0) {
-            //inizializing the hotel-array
-            hotelCount = stoi(line);
-            hotels = new Hotel[hotelCount];
-        }
-        else if (lineCount == 1) {
-            endPointTime = stoi(line);
-        }
-        else {
-            Hotel temp;
-
-            int index = line.find(" ");
-            temp.time = stoi(line.substr(0, index));
-            temp.rating = stod(line.substr(index+1, line.length()));
-
-            hotels[lineCount - 2] = temp;
-        }
-        lineCount++;
-    }
-
-    fileStream.close();
-}
+int endPointTime;
+int hotelCount;
 
 void eval(int hotel, int prevs[], int level = 0) {
     // better path was already found
@@ -87,29 +58,68 @@ void eval(int hotel, int prevs[], int level = 0) {
     }
 }
 
+void evaluateFile() {
+    //// Loading data from file ////
+    hotelCount = 0;
+    endPointTime = 0;
 
-
-int main()
-{
     // get file from user
     std::string path;
     std::cout << "Bitte geben Sie den Pfad zur Testdatei ein: ";
     std::cin >> path;
 
     // load configs from given file
-    loadFromFile(path);
+    std::ifstream fileStream(path);
+    std::string line;
+    int lineCount = 0;
+
+    while (getline(fileStream, line)) {
+        if (lineCount == 0) {
+            //inizializing the hotel-array
+            hotelCount = stoi(line);
+            hotels = new Hotel[hotelCount];
+        }
+        else if (lineCount == 1) {
+            endPointTime = stoi(line);
+        }
+        else {
+            if (hotelCount < (lineCount - 2)) break;
+            Hotel temp;
+
+            int index = line.find(" ");
+            temp.time = stoi(line.substr(0, index));
+            temp.rating = stod(line.substr(index + 1, line.length()));
+
+            hotels[lineCount - 2] = temp;
+        }
+        lineCount++;
+    }
+
+    fileStream.close();
+    ////        ////
+
+
+    if (hotelCount <= 0) {
+        std::cout << "keine Hotels geladen!\n" << std::endl;
+        return;
+    }
+
     std::cout << "Reisezeit: " << endPointTime << "min\nHotels: " << hotelCount << std::endl;
     std::cout << "==========\nStarting Search..." << std::endl;
 
-
+    //
     bestPath = new int[maxDays];
+    bestPathRating = 0.0;
+    bestPathTime = 10;
 
-    int *heapArray = new int[5];
+    // start evaluation for the hotels reachable on the first day
+    int* heapArray = new int[5];
     for (int hotel = 0; hotel < hotelCount; hotel++) {
         if (hotels[hotel].time > maxDrivingTime) break;
         eval(hotel, heapArray);
     }
-    
+
+    // printing the result
     if (bestPathTime > 5) std::cout << "Kein Pfad gefunden" << std::endl;
     else {
         std::cout << "Bester Pfad: ";
@@ -117,12 +127,28 @@ int main()
             std::cout << std::to_string(bestPath[i]);
             if (i != bestPathTime) std::cout << " --> ";
         }
-            
+
         std::cout << " (schlechteste Bewertung: " << bestPathRating << ")" << std::endl;
     }
-    
+
     delete[] heapArray;
     delete[] hotels;
     delete[] bestPath;
+}
+
+int main()
+{
+    while (true) {
+        evaluateFile();
+
+
+        std::cout << "noch eine Datei? (y/n)";
+        char answer;
+        std::cin >> answer;
+        if (answer != 'y') {
+            break;
+        }
+        else std::cout << std::endl;
+    }
 }
 
