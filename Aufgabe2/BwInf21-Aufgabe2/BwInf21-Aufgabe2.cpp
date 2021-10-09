6f865d108a87813b14198a2dcd7b56aac62e4685
Tollpatsch314 <Tollpatsch@ANTWORT42>
2021-10-09 12:40:02 +0200

    kleine optimierung

--- Aufgabe2/BwInf21-Aufgabe2/BwInf21-Aufgabe2.cpp
+++ Aufgabe2/BwInf21-Aufgabe2/BwInf21-Aufgabe2.cpp
@@ -9,12 +9,12 @@
 #define maxDays 5
 
 struct Hotel {
-	double rating;
-	int time;
-};
+	uint8_t rating;
+	int16_t time;
+} __packed;
 
 int* bestPath;
-double bestPathRating;
+uint8_t bestPathRating;
 int bestPathTime;
 
 Hotel* hotels;
@@ -25,9 +25,12 @@ const char* header = "BwInf 2021 - Aufgabe 2 \"Vollgeladen\"\n\0";
 
 #ifdef CHRONOMETER
 std::chrono::high_resolution_clock::time_point start_point;
+unsigned long calltime;
 #endif
 
 void eval(int hotel, int prevs[], int level = 0) {
+
+	calltime++;
 
 	// better path was already found
 	if (hotels[hotel].rating <= bestPathRating) return;
@@ -38,7 +41,7 @@ void eval(int hotel, int prevs[], int level = 0) {
 	//evaluating the path
 	if (endPointTime - hotels[hotel].time <= maxDrivingTime) {
 		// searching the worst rating
-		double minRating = 10.0;
+		uint8_t minRating = 100;
 		for (int i = 0; i < level+1; i++) {
 			if (hotels[prevs[i]].rating < minRating) minRating = hotels[prevs[i]].rating;
 		}
@@ -64,11 +67,17 @@ void eval(int hotel, int prevs[], int level = 0) {
 	for (int i = hotel + 1; i < hotelCount; i++) {
 		if (hotels[i].time - hotels[hotel].time > maxDrivingTime) break;
 
-		eval(i, prevs, level + 1);
+		eval(i, prevs, level+1);
 	}
 }
 
 void evaluateFile() {
+
+	// measurement of time
+#ifdef CHRONOMETER
+	calltime = 0;
+	start_point = std::chrono::high_resolution_clock::now();
+#endif
 
 	//// Loading data from file ////
 	hotelCount = 0;
@@ -83,11 +92,6 @@ void evaluateFile() {
 	std::ifstream fileStream(path);
 	std::string line;
 	int lineCount = 0;
-
-	// measurement of time
-#ifdef CHRONOMETER
-	start_point = std::chrono::high_resolution_clock::now();
-#endif
 
 	while (getline(fileStream, line)) {
 		if (lineCount == 0) {
@@ -104,7 +108,7 @@ void evaluateFile() {
 
 			int index = line.find(" ");
 			temp.time = stoi(line.substr(0, index));
-			temp.rating = stod(line.substr(index + 1, line.length()));
+			temp.rating = (uint8_t)(stod(line.substr(index + 1, line.length())) * 10);
 
 			hotels[lineCount - 2] = temp;
 		}
@@ -125,7 +129,7 @@ void evaluateFile() {
 
 	//
 	bestPath = new int[maxDays];
-	bestPathRating = 0.0;
+	bestPathRating = 0;
 	bestPathTime = 10;
 
 	// start evaluation for the hotels reachable on the first day
@@ -144,7 +148,7 @@ void evaluateFile() {
 			if (i != bestPathTime) std::cout << " --> ";
 		}
 
-		std::cout << " (schlechteste Bewertung: " << bestPathRating << ")" << std::endl;
+		std::cout << " (schlechteste Bewertung: " << ((double)bestPathRating) / 10.0 << ")" << std::endl;
 	}
 
 #ifdef CHRONOMETER
@@ -157,6 +161,7 @@ void evaluateFile() {
 
 	std::cout << "ben\224tigte Berechnungszeit: " << (min != 0 ? std::to_string(min) + " min, " : "");
 	std::cout << (sec != 0 ? std::to_string(sec) + " s, " : "") << mili_sec << " ms, " << micro_sec << " \xE6s" << std::endl;
+	std::cout << "Anzahl an Aufrufen: " << calltime << std::endl;
 #endif
 
 	delete[] heapArray;
