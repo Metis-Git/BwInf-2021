fab4af1aeebc93bfc881dda7f56f0784c7323d8a
Tollpatsch314 <76736177+Tollpatsch314@users.noreply.github.com>
2021-10-12 17:23:26 +0200

    Update BwInf21-Aufgabe2.cpp

--- Aufgabe2/BwInf21-Aufgabe2/BwInf21-Aufgabe2.cpp
+++ Aufgabe2/BwInf21-Aufgabe2/BwInf21-Aufgabe2.cpp
@@ -29,9 +29,9 @@ unsigned long calltime;
 #endif
 
 void eval(int hotel, int prevs[], int level = 0) {
-
+#ifdef CHRONOMETER
 	calltime++;
-
+#endif
 	// better path was already found
 	if (hotels[hotel].rating <= bestPathRating) return;
 
@@ -59,7 +59,7 @@ void eval(int hotel, int prevs[], int level = 0) {
 		}
 	}
 
-	// Zeitüberschreitung
+	// ZeitÃ¼berschreitung
 	if (level >= maxDays-1) return;
 
 
